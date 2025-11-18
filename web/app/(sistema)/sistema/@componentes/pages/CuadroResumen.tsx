fe85b280466d2ee85424f42b916aaa622c069a0f
4z4c <arturoyz2105@gmail.com>
2025-11-17 19:48:46 -0500

    ya

--- web/app/(sistema)/sistema/@componentes/pages/CuadroResumen.tsx
+++ web/app/(sistema)/sistema/@componentes/pages/CuadroResumen.tsx
@@ -1,5 +1,4 @@
-
-'use server'
+"use server";
 
 import { TablaGeneralLegacy } from "@/components/tables/TablaGeneraLegacy";
 import { columnasCuadroResumen } from "../types/columnasCuadroResumen";
@@ -7,27 +6,27 @@ import FormExcelExportarResumen from "../excel/FormExcelExportarResumen";
 import { config } from "@/config/config";
 
 const dataTablaTotal = await fetch(`${config.apiUrl}/operaciones/tabla-total-anio/cuadro/total`, {
-    cache: "no-store",
-    credentials: 'include',
-})
+  cache: "no-store",
+  credentials: "include",
+});
 
-const resTablaTotal = await dataTablaTotal.json()
-console.log('TABLA TOTAL: ' ,resTablaTotal)
+const resTablaTotal = await dataTablaTotal.json();
+console.log("TABLA TOTAL: ", resTablaTotal);
 
 export default async function CuadroResumen() {
-    return (
-        <div className="w-full">
-            <TablaGeneralLegacy
-                columns={columnasCuadroResumen}
-                data={resTablaTotal}
-                textAdd=""
-                filters={[]}
-                hideSearch={true}
-                hideAddButton={true}
-                hideCalculoTotal={true}
-                hideExpotarTabla={true}
-                formExportModal={<FormExcelExportarResumen />}
-            />
-        </div>
-    )
+  return (
+    <div className="w-full">
+      <TablaGeneralLegacy
+        columns={columnasCuadroResumen as any}
+        data={resTablaTotal}
+        textAdd=""
+        filters={[]}
+        hideSearch={true}
+        hideAddButton={true}
+        hideCalculoTotal={true}
+        hideExpotarTabla={true}
+        formExportModal={<FormExcelExportarResumen />}
+      />
+    </div>
+  );
 }
